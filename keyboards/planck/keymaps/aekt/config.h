0cb3045d0f3f14e0e18dddffdd6f5f61f7c36f9f
Yusan <yusan-23@hotmail.com>
2020-02-16 00:42:40 +0800

    planck: Add a basic layout with LEDs off
    By default, the ez will turn on left and right LEDs when layer is `3`,
    `4`, and `6`, which are the Lower, Raise, and Adjust layers
    respectively. Three dummy layers are placed to indice this.

---
+++ keyboards/planck/keymaps/aekt/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+#endif
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+
+#define RETRO_TAPPING
+#define FORCE_NKRO
+#undef IGNORE_MOD_TAP_INTERRUPT
+

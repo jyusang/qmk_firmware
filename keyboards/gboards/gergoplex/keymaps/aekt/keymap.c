435b9ae91c8f25290dfe6db20637ef0926e61b25
Yusan <jyusang@protonmail.com>
2021-12-07 21:36:34 +0800

    gergoplex: Support single spacebar setting
    - Move quotes to symbol layer
    - Move tab to outer right thumb
    - Move modifiers away from function layer

--- keyboards/gboards/gergoplex/keymaps/aekt/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/aekt/keymap.c
@@ -2,6 +2,7 @@
 
 enum layers {
     _QWERTY,
+    _COLEMAK,
     _SYMBOL,
     _COMMON,
     _POINT,
@@ -9,6 +10,7 @@ enum layers {
 };
 
 #define QWERTY DF(_QWERTY)
+#define COLEMAK DF(_COLEMAK)
 
 #define POINT MO(_POINT)
 #define FUNCT MO(_FUNCT)
@@ -20,9 +22,8 @@ enum layers {
 
 #define LG_BSPC MT(MOD_LGUI, KC_BSPC)
 #define LC_ESC MT(MOD_LCTL, KC_ESC)
-#define LS_TAB MT(MOD_LSFT, KC_TAB)
 #define SM_ENT LT(_SYMBOL, KC_ENT)
-#define CM_QUOT LT(_COMMON, KC_QUOT)
+#define CM_TAB LT(_COMMON, KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
@@ -30,31 +31,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    ,
         KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN ,
         LS_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , RS_SLSH ,
-                            LG_BSPC , LC_ESC  , KC_SPC  , LS_TAB  , SM_ENT  , CM_QUOT
+                            LG_BSPC , LC_ESC  , KC_SPC  , KC_SPC  , SM_ENT  , CM_TAB
+    ),
+    [_COLEMAK] = LAYOUT_gergoplex(
+        KC_Q    , KC_W    , KC_F    , KC_P    , KC_G    , KC_J    , KC_L    , KC_U    , KC_Y    , KC_SCLN ,
+        KC_A    , KC_R    , KC_S    , KC_T    , KC_D    , KC_H    , KC_N    , KC_E    , KC_I    , KC_O    ,
+        KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_K    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH ,
+                            LG_BSPC , LC_ESC  , KC_SPC  , KC_SPC  , SM_ENT  , CM_TAB
     ),
     [_SYMBOL] = LAYOUT_gergoplex(
         KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    ,
         KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN ,
         LS_GRV  , KC_LBRC , KC_RBRC , KC_UNDS , KC_MINS , KC_PLUS , KC_EQL  , KC_LCBR , KC_RCBR , RS_BSLS ,
-                            KC_LGUI , KC_LCTL , _______ , _______ , _______ , _______
+                            KC_QUOT , KC_DQUO , _______ , _______ , _______ , KC_RGUI
     ),
     [_COMMON] = LAYOUT_gergoplex(
         _______ , _______ , _______ , _______ , _______ , KC_MPRV , KC_VOLD , KC_VOLU , KC_MNXT , KC_MPLY ,
-        _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , _______ ,
-        KC_CAPS , _______ , _______ , _______ , _______ , KC_HOME , KC_PGDN , KC_PGUP , KC_END  , KC_DEL  ,
-                            POINT   , FUNCT   , _______ , _______ , _______ , _______
+        POINT   , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , FUNCT   ,
+        KC_CAPS , _______ , _______ , _______ , _______ , KC_HOME , KC_PGDN , KC_PGUP , KC_END  , KC_LSFT ,
+                            KC_LGUI , KC_LCTL , _______ , _______ , KC_RCTL , _______
     ),
     [_POINT] = LAYOUT_gergoplex(
         _______ , _______ , KC_MS_U , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
         _______ , KC_MS_L , KC_MS_D , KC_MS_R , _______ , _______ , KC_BTN1 , KC_BTN2 , KC_BTN3 , _______ ,
         _______ , KC_ACL0 , KC_ACL1 , KC_ACL2 , _______ , KC_WH_L , KC_WH_D , KC_WH_U , KC_WH_R , _______ ,
-                            _______ , _______ , _______ , _______ , _______ , _______
+                            KC_LALT , _______ , _______ , _______ , _______ , KC_RALT
     ),
     [_FUNCT] = LAYOUT_gergoplex(
-        KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______ , _______ , _______ , _______ , _______ , _______ ,
-        KC_F5   , KC_F6   , KC_F7   , KC_F8   , _______ , _______ , AU_OFF  , AU_ON   , _______ , RESET   ,
-        KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , MU_OFF  , MU_ON   , _______ , KC_LSFT ,
-                            _______ , _______ , KC_LALT , KC_RALT , KC_RCTL , KC_RGUI
+        KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______ , _______ , QWERTY  , COLEMAK , _______ , RESET   ,
+        KC_F5   , KC_F6   , KC_F7   , KC_F8   , _______ , _______ , AU_OFF  , AU_ON   , _______ , _______ ,
+        KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , MU_OFF  , MU_ON   , _______ , _______ ,
+                            _______ , _______ , _______ , _______ , _______ , _______
     ),
     // clang-format on
 };
